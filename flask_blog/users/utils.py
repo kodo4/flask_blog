f3562316f6da044384427033a654ebae5dbca41c
Роман Кодочигов <kodochigovra@mail.ru>
2022-05-31 19:19:59 +0300

    fix PIL in utils

--- flask_blog/users/utils.py
+++ flask_blog/users/utils.py
@@ -1,6 +1,6 @@
 import os
 from secrets import token_hex
-from pil import Image
+from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
 from flask_blog import mail
